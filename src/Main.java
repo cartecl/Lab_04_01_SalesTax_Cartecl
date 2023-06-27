9da0b1bc20b4fef13550ff7b19ddd6acbd6c1e5d
chandlercarter <cartecl@mail.uc.edu>
2023-06-27 14:10:08 -0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    
-        System.out.printf("Hello and welcome!");
+        //Given the price of a purchase the program computes and outputs a 5% sales tax.
+        //Be sure to use output statements that clearly display the price and the computed sales tax
+        //(No conditions here, the sales tax is always the same rate.)
+
+        double doublePrice = 10.50;
+        double doubleTax = 0.05;
+        double doubleSaleTax = doublePrice * doubleTax;
+
+        System.out.println("Your sales tax is: " +doubleSaleTax);
+
         }
     }
